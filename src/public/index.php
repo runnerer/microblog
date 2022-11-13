af285b151c38bc2bfd9f3e537c81e2789a8defce
runnerer <pgeorgiev356@gmail.com>
2022-11-13 21:02:34 +0200

    add basic setup and slim framework

---
+++ src/public/index.php
@@ -0,0 +1,24 @@
+<?php
+
+$config = [];
+
+require '../vendor/autoload.php';
+require 'App/Config/app.php';
+require 'App/Config/db.php';
+
+$app = new \Slim\App(['settings' => $config]);
+
+$container = $app->getContainer();
+
+$container['db'] = function ($c) {
+    $db = $c['settings']['db'];
+    $pdo = new PDO('mysql:host=' . $db['host'] . ';dbname=' . $db['dbname'],
+        $db['user'], $db['pass']);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    return $pdo;
+};
+
+require 'App/Config/routes.php';
+
+$app->run();
